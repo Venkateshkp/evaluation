e10503800f82ca4f1f7612813cab90dbd586ff4d
Venkateshkp <venkatpandu938@gmail.com>
2022-05-28 18:06:55 +0530

    made some changes

--- Todo/Todo.js
+++ Todo/Todo.js
@@ -8,19 +8,18 @@
 //   body: JSON.stringify(data),
 // })
 
-const title = document.getElementById("heading");
 function getData(){
-
-fetch('http://localhost:3000/name', {
-  method: 'POST', // or 'PUT'
-  headers: {
-    'Content-Type': 'db/json',
-  },
-  body: JSON.stringify(title),
+    const title = document.getElementById("heading").value;
+    fetch('http://localhost:3000/name', {
+        method: 'POST', // or 'PUT'
+        headers: {
+        'Content-Type': 'db/json',
+    },
+    body: JSON.stringify(title),
 })
 .then(response => response.json())
-.then(data => {
-  console.log('Success:', data);
+.then(title => {
+  console.log('Success:', title);
 })
 .catch((error) => {
   console.error('Error:', error);
