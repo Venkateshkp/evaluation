c4f27b94d4e51fd8132b6822a92ce78f072de01f
Venkateshkp <venkatpandu938@gmail.com>
2022-05-28 23:19:45 +0530

    add some changes

--- Todo/Todo.js
+++ Todo/Todo.js
@@ -1,28 +1,48 @@
-// const data = { username: 'example' };
+var form = document.getElementById("form");
 
-// fetch(' http://localhost:3000/profile', {
-//   method: 'POST', // or 'PUT'
-//   headers: {
-//     'Content-Type': 'application/json',
-//   },
-//   body: JSON.stringify(data),
-// })
 
-function getData(){
-    const title = document.getElementById("heading").value;
-    fetch('http://localhost:3000/name', {
-        method: 'POST', // or 'PUT'
-        headers: {
-        'Content-Type': 'db/json',
-    },
-    body: JSON.stringify(title),
-})
-.then(response => response.json())
-.then(title => {
-  console.log('Success:', title);
-})
-.catch((error) => {
-  console.error('Error:', error);
-});
+function checkFunction(){
+    var status = "False"; 
+    var checkbox = document.getElementById("myCheck");
+    if(checkbox.checked == true){
+        status = "True";
+    }
+    return status;
 }
-getData();
+
+form.addEventListener("submit", function(e){
+    e.preventDefault();
+    var heading1 = document.getElementById("heading").value;
+    var value = checkFunction();
+    fetch("http://localhost:3000/name", {
+        method: "POST",
+        body:JSON.stringify({
+            title: heading1,
+            status: value
+        }),
+        headers:{
+            "Content-Type": "application/json; charset=UTF-8"
+        }
+    })
+    .then(function(response){
+        return response.json()
+    })
+    .then(function (data){
+        console.log(data);
+    })
+    async function getData(){
+        var res = await fetch("http://localhost:3000/name");
+        var data = await res.json();
+        displayData(data);
+    }
+    getData();
+    var container = document.getElementById("container");
+    function displayData(item){
+        var title = document.createElement("p");
+        title.textContent = "Title: " + item.title;
+        var status = document.createElement("p");
+        status.textContent = "Status: " + item.status;
+    
+        container.append(title, status);
+    }
+})
